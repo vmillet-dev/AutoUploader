8ea2fe99f7ba82aafe3d4d2f28e78c38e658537a
Valentin MILLET <valentin.millet39@gmail.com>
2024-10-31 18:55:21 +0100

    Add tiktok service class with custom selenium webdriver

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import logging
 
+from tiktok_service import TiktokService
+
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    service = TiktokService()
+    service.get_videos_by_tag("test")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
